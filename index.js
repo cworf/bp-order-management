f2958d68fbb7b6d249f94a584e021a4af24e7514
Colin Worf <colin.worf@gmail.com>
2019-11-15 14:46:16 -0700

    create base code structure

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require(axios)
+
+exports.orderHandler = async(req, res) => {
+  const orderID = req.id
+
+  const orderItems = await axios.get('https://www.bluepeaks.biz/api/v1/orders/' + orderID + '/items',{
+    headers: {
+      "X-AC-Auth-Token": "512cf4799d9ab8cdbdcb6c74ce2444b4",
+    }
+  })
+
+  const promiseArray = orderItems.items.map(item => axios.get('https://www.bluepeaks.biz/api/v1/product/' + item.id + '/items',{
+      headers: {
+        "X-AC-Auth-Token": "512cf4799d9ab8cdbdcb6c74ce2444b4",
+      }
+    })
+  )
+
+  const productArray = (
+    await Promise.all(promiseArray)
+  ).map(prodRes => prodRes.data)
+
+  const orderProducts = {}
+//get the product information for each order item
+  for (var i = 0; i < productArray.length; i++) {
+    const product = productArray[i].
+    orderProducts[product.item_number] = product
+  }
+
+}
